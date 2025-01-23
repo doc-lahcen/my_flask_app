445790be31b1804f14b5f09f9cd2d2545a0ff4aa
doc-lahcen <lahcen.belouaddane.doc@uhp.ac.ma>
2025-01-23 21:48:17 +0100

    db1

--- app/controllers/main_controller.py
+++ app/controllers/main_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, jsonify
+from flask import Blueprint, render_template, jsonify, request
 import mysql.connector
 from mysql.connector import Error
 
@@ -17,10 +17,11 @@ def test_db():
     connection = None
     try:
         connection = mysql.connector.connect(
-            host='localhost',
+            host='192.168.1.111',
             user='root',
             password='fsts@2025',
-            database='app_data'
+            database='app_data',
+            port=33006
         )
         if connection.is_connected():
             return jsonify({"message": "Connected to the database!"})
@@ -33,3 +34,30 @@ def test_db():
 @main.route('/presentation')
 def presentation():
     return render_template('presentation.html')
+
+@main.route('/add_user', methods=['GET', 'POST'])
+def add_user():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        
+        connection = None
+        try:
+            connection = mysql.connector.connect(
+                host='localhost',
+                user='root',
+                password='fsts@2025',
+                database='app_data',
+                port=33006
+            )
+            cursor = connection.cursor()
+            cursor.execute("INSERT INTO users (username, password, email) VALUES (%s, %s, %s)", (username, password, email))
+            connection.commit()
+            return jsonify({"message": "User added successfully!"})
+        except Error as e:
+            return jsonify({"error": str(e)})
+        finally:
+            if connection and connection.is_connected():
+                connection.close()
+    return render_template('add_user.html')
